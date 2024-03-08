42e21ef4cba9a815a40a7761681eae2ce4f20d28
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-03-07 21:39:20 -0600

    clases de las tablas, conexion a la bd y controlador de cargo

--- CAEZ.Administracion.EN/Cargo.cs
+++ CAEZ.Administracion.EN/Cargo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,13 @@ namespace CAEZ.Administracion.EN
 {
     public class Cargo
     {
+        [Key]
+        public int Id { get; set; }
+
+        //anotaciones de validacion
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo de caracteres 50")]
+        public string Nombre { get; set; } = string.Empty; //inicializo qeu es un string de logitud cero
 
     }
 }
